61bda79961c20f6b6c00fc1dcd7e2297f83bc806
Tyler McCraw <mccraw.tyler@gmail.com>
2015-06-21 15:08:49 -0400

    Initial Design
    Updated app theme to Material Design
    Set initial color scheme
    Created new project buttons

--- app/src/main/java/com/w3bshark/android_showcase/MainActivity.java
+++ app/src/main/java/com/w3bshark/android_showcase/MainActivity.java
@@ -4,6 +4,8 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.Window;
+import android.view.WindowManager;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -12,6 +14,11 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Window window = this.getWindow();
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        window.setStatusBarColor(this.getResources().getColor(R.color.statusbar));
     }
 
     @Override
